5accb02ba352a73214a22b33b62292673264ee71
Preff <a.griaznovskiy@gmail.com>
2023-11-06 12:01:03 +0300

    removed temp const

--- bubblesort_array100K.js
+++ bubblesort_array100K.js
@@ -11,10 +11,8 @@ function bubbleSort(arr) {
     for (let i = 0; i < n; i++) {
         for (let j = 0; j < n - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
-                const temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
+                [arr[j], arr[j+1]] = [arr[j+1], arr[j]] 
+                }
         }
     }
 
