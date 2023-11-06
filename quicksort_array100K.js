d6231dfd002e765c8f5855dc5ea363a828297dc8
Preff <a.griaznovskiy@gmail.com>
2023-11-06 18:04:21 +0300

    removed filter

--- quicksort_array100K.js
+++ quicksort_array100K.js
@@ -6,23 +6,31 @@ for (let i = 0; i < 100000; i++) {
 
 // Функция для сортировки быстрым методом (QuickSort)
 function quickSort(arr) {
-    const n = arr.length;
-    if (n <= 1) {
+    if (arr.length <= 1) {
         return arr;
     }
-    
-    const pivot = arr[Math.floor(n / 2)];
-    const left = arr.filter(x => x < pivot);
-    const middle = arr.filter(x => x === pivot);
-    const right = arr.filter(x => x > pivot);
-    
-    return quickSort(left).concat(middle, quickSort(right));
+
+    const pivotIndex = Math.floor(arr.length / 2);
+    const pivot = arr[pivotIndex];
+
+    const left = [];
+    const right = [];
+    const middle = [pivot];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (i === pivotIndex) continue; // Пропустить опорный элемент
+        (arr[i] < pivot ? left : right).push(arr[i]);
+    }
+
+    return [...quickSort(left), ...middle, ...quickSort(right)];
 }
+
 
 // Измеряем время выполнения
 const startTime = new Date().getTime();
 const sortedArray = quickSort([...mylst]); // Сортируем и сохраняем отсортированный массив
 const endTime = new Date().getTime();
 
-console.log("Время выполнения: " + (endTime - startTime) + " миллисекунд");
+console.log("Время выполнения: " + ((endTime - startTime) / 1000) + " секунд");
 console.log("Отсортирован список длиной " + (mylst.length) + " элементов")
+console.log(sortedArray)
