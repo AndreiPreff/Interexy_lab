c30a843964a24fd12731fdd0b15720135bf1b429
Preff <a.griaznovskiy@gmail.com>
2023-11-15 11:20:42 +0300

    different animation methods and bubblesort of an array of 100k elements executed using WebWorker in a separate thread

---
+++ Animation&BubblesortWithWebWorker/script.js
@@ -0,0 +1,154 @@
+// Ожидание загрузки контента страницы перед выполнением функции
+document.addEventListener('DOMContentLoaded', async () => {
+    // Вызов функции для анимации и сортировки
+    animateAndSort();
+});
+
+// Функция для анимации движения квадрата вправо и влево
+function animateSquareThree() {
+    // Получение ссылок на элементы DOM
+    const squareThree = document.querySelector('.squareThree');
+    const rectangleThree = document.querySelector('.rectangleThree');
+    
+    // Получение ширины прямоугольника и квадрата
+    const rectangleWidth = rectangleThree.offsetWidth;
+    const squareWidth = squareThree.offsetWidth;
+
+    // Функция для движения квадрата вправо
+    function moveRight() {
+        return new Promise((resolve) => {
+            // Анимация движения вправо с использованием CSS transition
+            squareThree.style.transition = 'transform 2s ease-in-out';
+            squareThree.style.transform = `translateX(${rectangleWidth - squareWidth}px)`;
+
+            // Ожидание завершения анимации вправо
+            setTimeout(() => {
+                resolve();
+            }, 2000);
+        });
+    }
+
+    // Функция для движения квадрата влево
+    function moveLeft() {
+        return new Promise((resolve) => {
+            // Анимация движения влево с использованием CSS transition
+            squareThree.style.transition = 'transform 2s ease-in-out';
+            squareThree.style.transform = 'translateX(0)';
+
+            // Ожидание завершения анимации влево
+            setTimeout(() => {
+                resolve();
+            }, 2000);
+        });
+    }
+
+    // Функция для циклического вызова анимаций
+    async function animateLoop() {
+        // Бесконечный цикл анимации
+        while (true) {
+            // Анимация движения вправо
+            await moveRight();
+            
+            // Анимация движения влево
+            await moveLeft();
+        }
+    }
+
+    // Запуск циклической анимации
+    animateLoop();
+}
+
+// Функция для анимации движения квадрата squareFour вправо и влево с использованием Request Animation Frame
+function animateSquareFour() {
+    const squareFour = document.querySelector('.squareFour');
+    const rectangleFour = document.querySelector('.rectangleFour');
+    // Получение ширины прямоугольника (rectangleFour) и квадрата (squareFour)
+    const rectangleWidth = rectangleFour.offsetWidth;
+    const squareWidth = squareFour.offsetWidth;
+
+
+    // Устанавливаем начальные значения
+    let currentPosition = 0;
+    let direction = 1; // Направление движения: 1 - вправо, -1 - влево
+
+    // Функция анимации с использованием Request Animation Frame
+    function animate() {
+        // Перемещаем квадрат
+        currentPosition += direction * 2; // Скорость движения (2px за кадр)
+        squareFour.style.transform = `translateX(${currentPosition}px)`;
+
+        // Проверяем достижение границы прямоугольника
+        if (currentPosition + squareWidth > rectangleWidth || currentPosition < 0) {
+            // Меняем направление при достижении границы
+            direction *= -1;
+        }
+
+        // Продолжаем анимацию
+        requestAnimationFrame(animate);
+    }
+
+    // Запускаем анимацию
+    animate();
+}
+
+// Функция для анимации и сортировки
+async function animateAndSort() {
+    // Запуск анимаций для двух квадратов
+    animateSquareThree();
+    animateSquareFour();
+
+    // Генерация неотсортированного массива из 100000 элементов
+    const unsortedArray = generateRandomArray(100000);
+
+    // Вывод неотсортированного массива в консоль
+    console.log('Unsorted Array:', unsortedArray);
+
+    // Засекаем время начала сортировки
+    const startTime = new Date().getTime();
+
+    // Создаем Web Worker для сортировки массива
+    const sortingWorker = new Worker('sortingWorker.js');
+
+    // Обработчик сообщений от Web Worker
+    sortingWorker.onmessage = function (e) {
+        // Получаем отсортированный массив из сообщения
+        const { sortedArray } = e.data;
+
+        // Вывод отсортированного массива в консоль
+        console.log('Sorted Array:', sortedArray);
+
+        // Засекаем время окончания сортировки
+        const endTime = new Date().getTime();
+
+        // Получаем элемент для вывода результатов сортировки
+        const sortedArrayElement = document.getElementById('sortingSection');
+
+        // Создаем элемент для отображения времени выполнения сортировки
+        const timeTakenElement = document.createElement('p');
+        timeTakenElement.textContent = `BubbleSort has taken: ${(endTime - startTime) / 1000} seconds to complete`;
+
+        // Добавляем элемент в раздел для результатов сортировки
+        sortedArrayElement.appendChild(timeTakenElement);
+
+        // Создаем элемент для отображения количества элементов в отсортированном массиве
+        const lengthElement = document.createElement('p');
+        lengthElement.textContent = `Array of ${sortedArray.length} elements has been sorted`;
+
+        // Добавляем элемент в раздел для результатов сортировки
+        sortedArrayElement.appendChild(lengthElement);
+    };
+
+    // Отправляем неотсортированный массив в Web Worker для сортировки
+    sortingWorker.postMessage(unsortedArray);
+}
+
+// Функция для генерации случайного массива заданного размера
+function generateRandomArray(size) {
+    const randomArray = [];
+    for (let i = 0; i < size; i++) {
+        // Генерация случайного числа и добавление в массив
+        randomArray.push(Math.floor(Math.random() * 100000) + 1);
+    }
+    return randomArray;
+}
+
