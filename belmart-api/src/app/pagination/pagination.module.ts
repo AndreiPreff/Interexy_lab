b2a2f8e565d5d1f5d0c42bfd35a06cd2597a8584
Preff <a.griaznovskiy@gmail.com>
2023-12-20 16:29:24 +0300

    edit 2

--- belmart-api/src/app/pagination/pagination.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PaginationService } from './pagination.service';
-import {PrismaService} from "../../libs/prisma.service";
-
-@Module({
-  providers: [PaginationService, PrismaService],
-  exports: [PaginationService]
-})
-export class PaginationModule {}
